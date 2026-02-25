a6c9c96e3cdbd2665bd978b658e52a21bddc2a34
ililog <142715753+ililog@users.noreply.github.com>
2026-02-25 23:16:11 +0200

    Delete lab2_extra/lab2_extra directory

--- lab2_extra/lab2_extra/Compare.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Movie.h"
-
-int movie_compare_name(Movie &M1, Movie &M2);
-int movie_compare_year(Movie& M1, Movie& M2);
-int movie_compare_score(Movie& M1, Movie& M2);
-int movie_compare_length(Movie& M1, Movie& M2);
-int movie_compare_passed_years(Movie& M1, Movie& M2);
